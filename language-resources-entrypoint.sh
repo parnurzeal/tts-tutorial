ab6095a2eced7ce35944604c8623afb3e48b109c
twattanavekin <twattanavekin@google.com>
2017-07-10 14:15:21 +0800

    Add generic voice building option

--- language-resources-entrypoint.sh
+++ language-resources-entrypoint.sh
@@ -44,4 +44,45 @@ elif [ "$1" == "run_voice" ] && [ "$#" -eq 5 ]; then
     -eval festvox/goog_${LANGUAGE}_unison_cg.scm \
     -eval "(voice_goog_${LANGUAGE}_unison_cg)" \
     > ${OUTPUT_WAV}
+elif [ "$1" == "build_generic_voice" ] && [ "$#" -eq 7 ]; then
+  echo "Build generic voice on Docker..."
+  LANGUAGE=$2
+  WAV_TAR=$3
+  PHONOLOGY_JSON=$4
+  LEXICON_SCM=$5
+  TEXT_LINE=$6
+  VOICE_TAR=$7
+
+  WAV_DIR=/tmp/wav
+  VOICE_DIR=/tmp/built_voice
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+  mkdir -p ${WAV_DIR} && tar xzvf ${WAV_TAR} -C ${WAV_DIR}
+
+  # Create langauge folder in festvox and move all necessary files to build voice.
+  # TODO: Add ENV VAR for langauge-resources in Dockerfile
+  FESTVOX_LANG_FOLDER=/usr/local/src/language-resources/${LANGUAGE}/festvox
+  mkdir -p ${FESTVOX_LANG_FOLDER}
+  cp ${PHONOLOGY_JSON} ${FESTVOX_LANG_FOLDER}/ipa_phonology.json
+  cp ${LEXICON_SCM} ${FESTVOX_LANG_FOLDER}/lexicon.scm
+  cp ${TEXT_LINE} ${FESTVOX_LANG_FOLDER}/txt.done.data
+  # end
+
+  # Temporary operation to filter in only lines that we have .wav files.
+  echo "Filter in only lines that we have .wav files..."
+  for i in $(ls ${WAV_DIR}) ; do
+    grep "$(echo $i | sed 's/.wav//g')" ${LANGUAGE}/festvox/txt.done.data >> /tmp/text
+  done
+  cp /tmp/text ${LANGUAGE}/festvox/txt.done.data
+  # end
+
+  echo "Add -x to better debugging on all scripts..."
+  sed -i '1s/bash/bash -x/g' ./utils/build_festvox_voice.sh
+  sed -i '69i sed -i "1s/sh/sh -x/g" ${VOICE_DIR}/bin/*' ./utils/build_festvox_voice.sh
+  ./utils/build_festvox_voice.sh ${WAV_DIR} ${LANGUAGE} ${VOICE_DIR}
+
+  cd ${VOICE_DIR} && tar czvf ${VOICE_TAR} .
+
 fi
